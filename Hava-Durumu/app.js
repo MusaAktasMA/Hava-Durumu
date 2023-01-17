49f77cef8f7ba1bdf641e0fe8e7099eee1310e88
Musa Aktaş <117383627+MusaAktasMA@users.noreply.github.com>
2023-01-17 14:31:28 +0300

    Hava Durumu
    Hava Durumu

---
+++ Hava-Durumu/app.js
@@ -0,0 +1,36 @@
+const url = 'https://api.openweathermap.org/data/2.5/'
+const key = '33c889f9dc98fdd5f8c36fc022657849'
+
+const setQuery = (e) => {
+    if(e.keyCode == '13')
+    getResult(searchBar.value)
+}
+
+const getResult = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+    fetch(query)
+    .then(weather => {
+        return weather.json()
+    })
+    .then(displayResult)
+}
+
+const displayResult = (result) => {
+    let city = document.querySelector('.city')
+    city.innerText = `${result.name},${result.sys.country}`
+
+    let temp = document.querySelector('.temp')
+    temp.innerText = `${Math.round(result.main.temp)}°C`
+
+    let desc = document.querySelector('.desc')
+    desc.innerText = result.weather[0].description
+
+    let minmax = document.querySelector('.minmax')
+    minmax.innerText = `${Math.round(result.main.temp_min)}°c / ${Math.round(result.main.temp_max)}°c`
+}
+
+const searchBar = document.getElementById('searchBar')
+searchBar.addEventListener('keypress',setQuery)
+
+
+
